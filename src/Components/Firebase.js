850f52fdbd4fe885beeaf98f499ae7fa590cb264
itsoliviasparks <itsoliviasparks@gmail.com>
2023-02-11 12:55:33 -0500

    added firebase

---
+++ src/Components/Firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDqq2VjodwBxMAhbgu6ewQ1UiVjMJ0AX7I",
+  authDomain: "backronym-generator-7a759.firebaseapp.com",
+  projectId: "backronym-generator-7a759",
+  storageBucket: "backronym-generator-7a759.appspot.com",
+  messagingSenderId: "1060183991889",
+  appId: "1:1060183991889:web:584b110bd2da5d28877a70"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

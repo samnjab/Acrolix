9ab18bd077dbb7886b9c227e9dbd10e64d7a39f5
samjaberi <samanjaberi.sj@gmail.com>
2023-02-19 16:02:55 -0500

    debugged the save function on edit

--- src/Components/SavedBackronyms.js
+++ src/Components/SavedBackronyms.js
@@ -87,7 +87,8 @@ const SavedBackronyms = ({activeKey, endpoint}) => {
     }
     const handleSave = (backronym) => {
         const database = getDatabase(firebase)
-        set(ref(database, endpoint + activeKey + backronym.key), backronym)
+        console.log('new backronym is', backronym.data)
+        set(ref(database, endpoint + activeKey + `/${backronym.key}`), backronym.data)
     }
     return (
         <>
